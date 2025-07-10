49d8150e6199c2e4bf1613832c4eb6702028df34
sayandas1694 <sayandas1694@gmail.com>
2025-07-10 09:25:14 +0530

    big changes

---
+++ server/server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express'); // Express module included
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const authRoutes = require('./src/routes/authRoutes');
+const linksRoutes = require('./src/routes/linksRoutes');
+const userRoutes = require('./src/routes/userRoutes');
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Database connected'))
+    .catch(error => console.log(error));
+
+const app = express(); // Instance of express application
+
+app.use(express.json()); // Middleware
+app.use(cookieParser()); // Middleware
+
+const corsOptions = {
+    origin: process.env.CLIENT_ENDPOINT,
+    credentials: true
+};
+app.use(cors(corsOptions));
+
+app.use('/auth', authRoutes);
+app.use('/links', linksRoutes);
+app.use('/users', userRoutes);
+
+const PORT = 5001;
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log('Server not started: ', error);
+    } else {
+        console.log(`Server is running on http://localhost:${PORT}`)
+    }
+});
